c722a5ea418c39e6a0a3c8e231a3cd2a0f6134ce
Kolko Valentin <valiant9191@gmail.com>
2021-01-17 18:23:59 +0300

    add fields for changing font color

--- script.js
+++ script.js
@@ -9,4 +9,25 @@ const button1 = document.querySelector('.button')
 button1.addEventListener('click', ()=>textOnPicture.innerHTML=inputFormJs.value )
 
 
+// font-colors
+const fontColor1=document.querySelector('.color1');
+const fontColor2=document.querySelector('.color2');
+const fontColor3=document.querySelector('.color3');
+const fontColor4=document.querySelector('.color4');
+const fontColor5=document.querySelector('.color5');
+
+
+
+const message=document.querySelector('.message')
+const style=getComputedStyle(fontColor2)
+console.log(style.backgroundColor)
+
+fontColor1.addEventListener('click', ()=>{message.style.color= getComputedStyle(fontColor1).backgroundColor})
+fontColor2.addEventListener('click', ()=>{message.style.color= getComputedStyle(fontColor2).backgroundColor})
+fontColor3.addEventListener('click', ()=>{message.style.color=getComputedStyle(fontColor3).backgroundColor})
+fontColor4.addEventListener('click', ()=>{message.style.color=getComputedStyle(fontColor4).backgroundColor})
+fontColor5.addEventListener('click', ()=>{message.style.color=getComputedStyle(fontColor5).backgroundColor})
+
+
+
 
